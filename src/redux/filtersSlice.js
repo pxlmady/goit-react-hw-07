2af3af36d5950987ea83dd2d09c71c81a78f1a8e
pxlmady <yoplufre@gmail.com>
2024-04-22 13:03:19 +0300

    fix-01

--- src/redux/filtersSlice.js
+++ src/redux/filtersSlice.js
@@ -12,4 +12,4 @@ const filtersSlice = createSlice({
   },
 });
 export const { changeFilter } = filtersSlice.actions;
-export const { filtersReducer } = filtersSlice.reducer;
+export const filtersReducer = filtersSlice.reducer;
